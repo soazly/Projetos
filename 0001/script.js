27f3d7513cc7609180157506208e0190b657b3ef
Soaz <soazlycoder@gmail.com>
2024-08-04 11:47:53 -0300

    Initial commit

---
+++ 0001/script.js
@@ -0,0 +1,8 @@
+const lastOne = document.querySelector('.last-one')
+
+const myObserver = new IntersectionObserver((entries) => {
+    console.log(entries)
+})
+
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Projetos
+ Treinando Html CSS
