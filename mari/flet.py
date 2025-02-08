b4cf7e57016b984d9ae2aab2c40eaf8508cc0655
Soaz <soazlycoder@gmail.com>
2025-02-08 16:04:19 -0300

    excluir

--- mari/flet.py
+++
@@ -1,7 +0,0 @@
-from flet import *
-
-def main(page: Page):
-    pass
-
-app(target=main,view=WEB_BROWSER)
-
--- mari/index.py
+++
@@ -1,7 +0,0 @@
-meta = 50000
-venda = 3000
-
-if meta < venda:
-    print('Bati a meta {} vendas'.format(venda))
-else:
-    print('Vish, não bateu a meta')
